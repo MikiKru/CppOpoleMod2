1ae674e541b349cd92b8844ffa32cc43ccaf84f4
MikiKru <michal_kruczkowski@o2.pl>
2019-11-10 09:52:54 +0100

    cpp mod2 v0

---
+++ cmake-build-debug/CMakeFiles/CPP_mod2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_mod2.dir/main.cpp.obj"
+  "CPP_mod2.exe"
+  "CPP_mod2.exe.manifest"
+  "CPP_mod2.lib"
+  "CPP_mod2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_mod2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include "time.h"
+
+using namespace std;
+
+
+
+int main() {
+
+    return 0;
+}
