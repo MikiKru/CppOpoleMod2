c7b6b703f7c3c9c1caa7835405958d2ace9d7f46
MikiKru <michal_kruczkowski@o2.pl>
2019-11-10 10:19:28 +0100

    cpp mod2 v1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,30 @@
 
 using namespace std;
 
+enum work_days{
+    poniedzialek,
+    wtorek,
+    sroda,
+    czwartek,
+    piatek
+};
 
+void enumTest(){
+    work_days today = piatek;
+    work_days nextDay = wtorek;
+//    work_days strangeDay = sobota;
+    cout << "Dzis jest " << today << " musze isc do szkoly :-(" << endl;
+    cout << "Jutro jest " << nextDay << " rowniez musze isc do szkoly :-(" << endl;
+
+    cout << "Index: " << today << " wartosc: " << work_days::poniedzialek << endl;
+    if(work_days::piatek == today){
+        cout << "jest piatek! " << endl;
+    } else {
+        cout << "jeszcze nie ma piatku " << endl;
+    }
+}
 
 int main() {
-
+    enumTest();
     return 0;
 }
